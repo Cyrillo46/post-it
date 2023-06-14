1bdfb8fd6b064e892878825913a28b62f8ee4e0d
Cyrillo46 <yahooisbetter46@gmail.com>
2023-06-13 20:51:14 -0400

    created home and navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
+
 function App() {
-  const title = "Welcome to Post-It!";
-  const likes = 50;
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} Times </p>
+        <Home />
       </div>
     </div>
   );
 }
+
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Posts</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Post-it</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
